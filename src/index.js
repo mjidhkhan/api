08d714b0acc0db81c776923ead94491ed7df7a22
muhammad <muhammad@g3t.co.uk>
2019-04-26 17:12:42 +0100

    change to default route

--- src/index.js
+++ src/index.js
@@ -12,42 +12,24 @@ app.listen(process.env.PORT, () =>
 );
 
 app.get('/', (req, res) => {
-    return res.send('Received a GET HTTP method');
-});
-/*
-app.post('/', (req, res) => {
-    return res.send('Received a POST HTTP method');
-});
+    var sql = 'SELECT * FROM course_details';
+    db.connect.query(sql, function(err, results) {
+        if (err) {
+            console.error(err);
+            res.statusCode = 500;
+            return res.json({ errors: [res.statusCode + ': Could not get photo'] });
+        }
+        // No results returned mean the object is not found
+        if (results.length === 0) {
+            // We are able to set the HTTP status code on the res object
+            res.statusCode = 404;
+            return res.json({ errors: ['User not found'] });
+        }
+        return res.json(results)
 
-app.put('/', (req, res) => {
-    return res.send('Received a PUT HTTP method');
-});
-
-app.delete('/', (req, res) => {
-    return res.send('Received a DELETE HTTP method');
-});
-
-
-
-
-
-app.get('/users', (req, res) => {
-    return res.send('GET HTTP method on user resource');
+    });
 });
 
-app.post('/users', (req, res) => {
-    return res.send('POST HTTP method on user resource');
-});
-
-app.put('/users', (req, res) => {
-    return res.send('PUT HTTP method on user resource');
-});
-
-app.delete('/users', (req, res) => {
-    return res.send('DELETE HTTP method on user resource');
-});
-
-*/
 app.get('/users/:id', (req, res, next) => {
     var uId = req.params.id;
     var sql = 'SELECT * FROM users WHERE id =?';
